7a0643ea48a00ebf24f75a5d80839aabebdee13c
dumbus <dumbusx@gmail.com>
2024-05-06 21:06:22 +0300

    feat: add local simulation for QR with QFT

---
+++ examples/2-2-quantum-register-with-qft.py
@@ -0,0 +1,51 @@
+import matplotlib.pyplot as plt
+from numpy import pi
+
+from qiskit import *
+from qiskit.providers.basic_provider import BasicSimulator
+
+REG_SIZE = 5
+SHOTS = 1000
+
+# initialize quantum register
+q_reg = QuantumRegister(size=REG_SIZE, name='q_reg')
+
+# initialize classical register
+c_reg = ClassicalRegister(size=REG_SIZE, name='c_reg')
+
+# initialize quantum curcuit with quantum register
+qc = QuantumCircuit(q_reg, c_reg)
+
+# Apply H gate to all qubits of q_reg
+qc.h(q_reg)
+# Measure all qubits of q_reg to c_reg
+# qc.measure(q_reg, c_reg)
+
+def qft_rotations(circuit, n):
+    if n == 0:
+        return circuit
+    n -= 1
+    circuit.h(n)
+    for qubit in range(n):
+        circuit.cp(pi/2**(n-qubit), qubit, n)
+    qft_rotations(circuit, n)
+
+def swap_registers(circuit, n):
+    for qubit in range(n//2):
+        circuit.swap(qubit, n-qubit-1)
+    return circuit
+
+def qft(circuit, n):
+    """QFT on the first n qubits in circuit"""
+    qft_rotations(circuit, n)
+    swap_registers(circuit, n)
+    return circuit
+
+# Apply QFT to qubits of q_reg
+qft(qc, REG_SIZE)
+
+# Measure all qubits of q_reg to c_reg
+qc.measure(q_reg, c_reg)
+
+qc.draw(output="mpl")
+plt.show()
---
+++ tools/qft.py
@@ -0,0 +1,23 @@
+def qft_rotations(circuit, n):
+    """Performs qft on the first n qubits in circuit (without swaps)"""
+    if n == 0:
+        return circuit
+    n -= 1
+    circuit.h(n)
+    for qubit in range(n):
+        circuit.cp(pi/2**(n-qubit), qubit, n)
+        # cp(circuit, pi/2**(n-qubit), qubit, n)
+    # At the end of our function, we call the same function again on
+    # the next qubits (we reduced n by one earlier in the function)
+    qft_rotations(circuit, n)
+
+def swap_registers(circuit, n):
+    for qubit in range(n//2):
+        circuit.swap(qubit, n-qubit-1)
+    return circuit
+
+def qft(circuit, n):
+    """QFT on the first n qubits in circuit"""
+    qft_rotations(circuit, n)
+    swap_registers(circuit, n)
+    return circuit
