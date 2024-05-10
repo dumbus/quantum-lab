2c55ffc5951ea57c33775e079c7a954536811009
dumbus <dumbusx@gmail.com>
2024-05-10 18:14:15 +0300

    feat: add add showcase of controlled gates

---
+++ controlled-gates/1-cx-gate.py
@@ -0,0 +1,46 @@
+import matplotlib.pyplot as plt
+
+from qiskit import *
+from qiskit.quantum_info import Statevector
+from qiskit.visualization import plot_bloch_multivector
+
+from qiskit_ibm_runtime import QiskitRuntimeService
+from qiskit_aer import AerSimulator
+
+SHOTS = 2000
+
+q_reg = QuantumRegister(size=4, name='q_reg')
+c_reg = ClassicalRegister(size=4, name='c_reg')
+qc = QuantumCircuit(q_reg, c_reg)
+
+# Set q_reg[2] and q_reg[3] to |1>
+qc.x(q_reg[2])
+qc.x(q_reg[3])
+
+# Draw state of Qubits on Bloch Sphere before experiment
+state = Statevector(qc)
+plot = plot_bloch_multivector(state, title='Initital State', title_pad=-3)
+plot.show()
+
+# Apply CX-Gate with controlled qubit |0>
+qc.cx(q_reg[0], q_reg[1])
+
+# Apply CX-Gate with controlled qubit |1>
+qc.cx(q_reg[2], q_reg[3])
+
+# Draw state of Qubits on Bloch Sphere after Rx-gate
+state = Statevector(qc)
+plot = plot_bloch_multivector(state, title='State after CX-Gates', title_pad=-3)
+plot.show()
+
+qc.measure(q_reg, c_reg)
+qc.draw(output="mpl")
+plt.show()
+
+# ======================= Local Simulation =======================
+service = QiskitRuntimeService()
+backend = service.get_backend('ibm_kyoto')
+backend_sim = AerSimulator.from_backend(backend)
+result = backend_sim.run(qc, shots=SHOTS).result()
+counts = result.get_counts()
+print(counts)
---
+++ controlled-gates/2-1-crx-gate.py
@@ -0,0 +1,47 @@
+import matplotlib.pyplot as plt
+from numpy import pi
+
+from qiskit import *
+from qiskit.quantum_info import Statevector
+from qiskit.visualization import plot_bloch_multivector
+
+from qiskit_ibm_runtime import QiskitRuntimeService
+from qiskit_aer import AerSimulator
+
+SHOTS = 2000
+
+q_reg = QuantumRegister(size=4, name='q_reg')
+c_reg = ClassicalRegister(size=4, name='c_reg')
+qc = QuantumCircuit(q_reg, c_reg)
+
+qc.x(q_reg[2])
+qc.x(q_reg[3])
+
+# Draw state of Qubits on Bloch Sphere before experiment
+state = Statevector(qc)
+plot = plot_bloch_multivector(state, title='Initital State', title_pad=-3)
+plot.show()
+
+# Apply CRx-Gate with controlled qubit |0>
+qc.crx(pi/2, q_reg[0], q_reg[1])
+
+# Apply CRx-Gate with controlled qubit |1>
+qc.crx(pi/2, q_reg[2], q_reg[3])
+
+# Draw state of Qubits on Bloch Sphere after Rx-gate
+state = Statevector(qc)
+plot = plot_bloch_multivector(state, title='State after CRx-Gates', title_pad=-3)
+plot.show()
+
+qc.measure(q_reg, c_reg)
+qc.draw(output="mpl")
+plt.show()
+
+# ======================= Local Simulation =======================
+service = QiskitRuntimeService()
+backend = service.get_backend('ibm_kyoto')
+backend_sim = AerSimulator.from_backend(backend)
+transpiled_circuit = transpile(qc, backend_sim)
+result = backend_sim.run(transpiled_circuit, shots=SHOTS).result()
+counts = result.get_counts()
+print(counts)
