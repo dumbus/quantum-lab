7cdac7e0007c9df18f66f543d3fcf9400fc50066
dumbus <dumbusx@gmail.com>
2024-06-02 15:50:45 +0300

    feat: add variants with n=51 for lab2

--- labs/modular-exponentiation.py
+++ labs/modular-exponentiation.py
@@ -5,17 +5,17 @@ from qiskit.visualization import plot_histogram
 from qiskit.providers.fake_provider import GenericBackendV2
 
 SHOTS = 2000
-NUM_QUBITS = 10
 
-q_reg_x = QuantumRegister(size=2, name='x')
-q_reg_y = QuantumRegister(size=4, name='y')
-c_reg = ClassicalRegister(size=4, name='c')
-qc = QuantumCircuit(q_reg_x, q_reg_y, c_reg)
-
-# Генерация схемы для студента
 student_id = int(input("Номер варианта: "))
 
 if (student_id == 0):
+  q_reg_x = QuantumRegister(size=2, name='x')
+  q_reg_y = QuantumRegister(size=4, name='y')
+  c_reg = ClassicalRegister(size=4, name='c')
+  qc = QuantumCircuit(q_reg_x, q_reg_y, c_reg)
+
+  NUM_QUBITS = 10
+
   qc.h(q_reg_x[0])
   qc.h(q_reg_x[1])
 
@@ -42,7 +42,15 @@ if (student_id == 0):
   qc.draw(output="mpl")
   plt.show()
 
-elif (student_id >= 1 and student_id <= 10):
+elif (student_id >= 1 and student_id <= 5):
+  ### N = 15, a = 13 ###
+  q_reg_x = QuantumRegister(size=2, name='x')
+  q_reg_y = QuantumRegister(size=4, name='y')
+  c_reg = ClassicalRegister(size=4, name='c')
+  qc = QuantumCircuit(q_reg_x, q_reg_y, c_reg)
+
+  NUM_QUBITS = 10
+
   qc.h(q_reg_x[0])
   qc.h(q_reg_x[1])
 
@@ -65,7 +73,38 @@ elif (student_id >= 1 and student_id <= 10):
   qc.barrier()
   qc.measure(q_reg_y, c_reg)
 
-elif (student_id >= 11 and student_id <= 20):
+elif (student_id >= 6 and student_id <= 10):
+  ### N = 51, a = 16 ###
+  q_reg_x = QuantumRegister(size=1, name='x')
+  q_reg_y = QuantumRegister(size=6, name='y')
+  c_reg = ClassicalRegister(size=6, name='c')
+  qc = QuantumCircuit(q_reg_x, q_reg_y, c_reg)
+
+  NUM_QUBITS = 13
+
+  qc.h(q_reg_x[0])
+
+  # Block 1
+  qc.x(q_reg_y[5])
+
+  qc.barrier()
+
+  # Block 2
+  qc.cx(q_reg_x[0], q_reg_y[5])
+  qc.cx(q_reg_x[0], q_reg_y[1])
+
+  qc.barrier()
+  qc.measure(q_reg_y, c_reg)
+
+elif (student_id >= 11 and student_id <= 15):
+  ### N = 15, a = 2 ###
+  q_reg_x = QuantumRegister(size=2, name='x')
+  q_reg_y = QuantumRegister(size=4, name='y')
+  c_reg = ClassicalRegister(size=4, name='c')
+  qc = QuantumCircuit(q_reg_x, q_reg_y, c_reg)
+
+  NUM_QUBITS = 10
+
   qc.h(q_reg_x[0])
   qc.h(q_reg_x[1])
 
@@ -88,7 +127,31 @@ elif (student_id >= 11 and student_id <= 20):
   qc.barrier()
   qc.measure(q_reg_y, c_reg)
 
-elif (student_id >= 21 and student_id <= 30):
+elif (student_id >= 16 and student_id <= 20):
+  ### N = 51, a = 35 ###
+  q_reg_x = QuantumRegister(size=1, name='x')
+  q_reg_y = QuantumRegister(size=6, name='y')
+  c_reg = ClassicalRegister(size=6, name='c')
+  qc = QuantumCircuit(q_reg_x, q_reg_y, c_reg)
+
+  NUM_QUBITS = 13
+
+  qc.h(q_reg_x[0])
+
+  # Block 1
+  qc.x(q_reg_y[5])
+
+  qc.barrier()
+
+  # Block 2
+  qc.cx(q_reg_x[0], q_reg_y[4])
+  qc.cx(q_reg_x[0], q_reg_y[0])
+
+  qc.barrier()
+  qc.measure(q_reg_y, c_reg)
+
+elif (student_id >= 21 and student_id <= 25):
+  ### N = 15, a = 8 ###
   qc.h(q_reg_x[0])
   qc.h(q_reg_x[1])
 
@@ -122,6 +185,31 @@ elif (student_id >= 21 and student_id <= 30):
   qc.ccx(q_reg_x[0], q_reg_y[1], q_reg_y[3])
   qc.ccx(q_reg_x[0], q_reg_y[3], q_reg_y[1])
   qc.ccx(q_reg_x[0], q_reg_y[1], q_reg_y[3])
+
+  qc.barrier()
+  qc.measure(q_reg_y, c_reg)
+
+elif (student_id >= 26 and student_id <= 30):
+  # N = 51, a = 50
+  q_reg_x = QuantumRegister(size=1, name='x')
+  q_reg_y = QuantumRegister(size=6, name='y')
+  c_reg = ClassicalRegister(size=6, name='c')
+  qc = QuantumCircuit(q_reg_x, q_reg_y, c_reg)
+
+  NUM_QUBITS = 13
+
+  qc.h(q_reg_x[0])
+
+  # Block 1
+  qc.x(q_reg_y[5])
+
+  qc.barrier()
+
+  # Block 2
+  qc.cx(q_reg_x[0], q_reg_y[5])
+  qc.cx(q_reg_x[0], q_reg_y[4])
+  qc.cx(q_reg_x[0], q_reg_y[1])
+  qc.cx(q_reg_x[0], q_reg_y[0])
 
   qc.barrier()
   qc.measure(q_reg_y, c_reg)
